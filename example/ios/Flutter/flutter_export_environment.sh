05751abdec5e5f9d2e54c22c31c4dacc19d744a5
tuyen nguyen <quangtuyen1993@gmail.com>
2025-07-11 10:30:37 +0700

    fix: change

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/hunghd/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hunghd/Developer/Workspace/flutter/flutter_pattern_formatter/example"
+export "FLUTTER_ROOT=/Users/tuyennguyen/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/tuyennguyen/flutter-proj/shareholder-app-git/flutter_pattern_formatter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
